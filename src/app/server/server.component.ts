90845cc86e4626fa47d44a96bf89c5d417470a97
milesdavis71 <hevesiistvan71@hotmail.com>
2022-04-23 16:00:58 +0200

    039 Styling Elements Dynamically with ngStyle - KÉSZ

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -8,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class ServerComponent implements OnInit {
   serverId = 66;
   serverStatus = 'offline';
-  constructor() {}
+
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'offline' : 'online';
+  }
 
   ngOnInit(): void {}
   getServerStatus() {
     return this.serverStatus;
+  }
+
+  getColor() {
+    return this.serverStatus === 'online' ? 'grey' : 'orange';
   }
 }
